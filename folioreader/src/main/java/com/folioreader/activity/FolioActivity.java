464c8d37de720485fc006ae558c5396b83852480
sameerkhader <sameerkhader018@gmail.com>
2016-03-30 17:48:33 +0530

    loading book onto recyclerView

--- folioreader/src/main/java/com/folioreader/activity/FolioActivity.java
+++ folioreader/src/main/java/com/folioreader/activity/FolioActivity.java
@@ -16,14 +16,13 @@
 package com.folioreader.activity;
 
 import android.content.res.AssetManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import com.folioreader.R;
+import com.folioreader.adapter.SpineReferenceAdapter;
 import com.folioreader.adapter.TOCAdapter;
 import com.folioreader.view.ConfigView;
 import com.folioreader.view.ConfigViewCallback;
@@ -42,81 +41,101 @@ import nl.siegmann.epublib.epub.EpubReader;
 public class FolioActivity extends AppCompatActivity implements ConfigViewCallback,
         FolioViewCallback {
 
-  public static final String INTENT_EPUB_ASSET_PATH = "com.folioreader.epub_asset_path";
-  private RecyclerView recyclerViewMenu;
-  private FolioView folioView;
-  private ConfigView configView;
-  private String mEpubAssetPath;
-  private ArrayList<TOCReference> mTocReferences = new ArrayList<>();
+    public static final String INTENT_EPUB_ASSET_PATH = "com.folioreader.epub_asset_path";
+    private RecyclerView recyclerViewMenu, recyclerViewContent;
+    private FolioView folioView;
+    private ConfigView configView;
+    private String mEpubAssetPath;
+    private Book mBook;
+    private ArrayList<TOCReference> mTocReferences = new ArrayList<>();
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.folio_activity);
-    mEpubAssetPath = getIntent().getStringExtra(INTENT_EPUB_ASSET_PATH);
-  }
-
-  @Override protected void onPostCreate(Bundle savedInstanceState) {
-    super.onPostCreate(savedInstanceState);
-    recyclerViewMenu = (RecyclerView) findViewById(R.id.recycler_view_menu);
-    folioView = (FolioView) findViewById(R.id.folio_view);
-    configView = (ConfigView) findViewById(R.id.config_view);
-    configRecyclerViews();
-    configFolio();
-  }
-
-  @Override public void onBackPressed() {
-    if (configView.isDragViewAboveTheLimit()) {
-      configView.moveToOriginalPosition();
-    } else {
-      super.onBackPressed();
-    }
-  }
-
-  @Override public void onBackgroundUpdate(int value) {
-    recyclerViewMenu.setBackgroundColor(value);
-    folioView.setBackgroundColor(value);
-  }
-
-  @Override public void onShadowAlpha(float alpha) {
-    folioView.updateShadowAlpha(alpha);
-  }
-
-  @Override public void showShadow() {
-    folioView.resetView();
-  }
-
-  @Override public void onShadowClick() {
-    configView.moveOffScreen();
-  }
-
-  private void configRecyclerViews() {
-    recyclerViewMenu.setLayoutManager(new LinearLayoutManager(this));
-    AssetManager assetManager = getAssets();
-    try {
-      InputStream epubInputStream = assetManager.open(mEpubAssetPath);
-      Book book = (new EpubReader()).readEpub(epubInputStream);
-      populateTableOfContents(book.getTableOfContents().getTocReferences(), 0);
-      TOCAdapter tocAdapter = new TOCAdapter(mTocReferences);
-      recyclerViewMenu.setAdapter(tocAdapter);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void populateTableOfContents(List<TOCReference> tocReferences, int depth) {
-    if (tocReferences == null) {
-      return;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.folio_activity);
+        mEpubAssetPath = getIntent().getStringExtra(INTENT_EPUB_ASSET_PATH);
+        loadBook();
     }
 
-    for (TOCReference tocReference : tocReferences) {
-      mTocReferences.add(tocReference);
-      populateTableOfContents(tocReference.getChildren(), depth + 1);
+    private void loadBook() {
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream epubInputStream = assetManager.open(mEpubAssetPath);
+            mBook = (new EpubReader()).readEpub(epubInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private void configFolio() {
-    folioView.setFolioViewCallback(this);
-    configView.setConfigViewCallback(this);
-  }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        recyclerViewMenu = (RecyclerView) findViewById(R.id.recycler_view_menu);
+        folioView = (FolioView) findViewById(R.id.folio_view);
+        configView = (ConfigView) findViewById(R.id.config_view);
+        configRecyclerViews();
+        configFolio();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (configView.isDragViewAboveTheLimit()) {
+            configView.moveToOriginalPosition();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onBackgroundUpdate(int value) {
+        recyclerViewMenu.setBackgroundColor(value);
+        folioView.setBackgroundColor(value);
+    }
+
+    @Override
+    public void onShadowAlpha(float alpha) {
+        folioView.updateShadowAlpha(alpha);
+    }
+
+    @Override
+    public void showShadow() {
+        folioView.resetView();
+    }
+
+    @Override
+    public void onShadowClick() {
+        configView.moveOffScreen();
+    }
+
+    private void configRecyclerViews() {
+        recyclerViewMenu.setLayoutManager(new LinearLayoutManager(this));
+        if (mBook != null) {
+            populateTableOfContents(mBook.getTableOfContents().getTocReferences(), 0);
+            TOCAdapter tocAdapter = new TOCAdapter(mTocReferences);
+            recyclerViewMenu.setAdapter(tocAdapter);
+        }
+    }
+
+    private void populateTableOfContents(List<TOCReference> tocReferences, int depth) {
+        if (tocReferences == null) {
+            return;
+        }
+
+        for (TOCReference tocReference : tocReferences) {
+            mTocReferences.add(tocReference);
+            populateTableOfContents(tocReference.getChildren(), depth + 1);
+        }
+    }
+
+    private void configFolio() {
+        recyclerViewContent = (RecyclerView) folioView.findViewById(R.id.folio_element);
+        recyclerViewContent.setLayoutManager(new LinearLayoutManager(this));
+        if (mBook != null) {
+            SpineReferenceAdapter spineAdapter = new SpineReferenceAdapter(mBook.getSpine().getSpineReferences());
+            recyclerViewContent.setAdapter(spineAdapter);
+        }
+        folioView.setFolioViewCallback(this);
+        configView.setConfigViewCallback(this);
+    }
 
 }
