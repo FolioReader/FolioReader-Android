4b0da8daac93b065d13be6936c411a8e793e722b
Mahavir Jain <mahavir.jain@mobisys.in>
2016-07-04 12:32:14 +0530

    added highlight activity

---
+++ folioreader/src/main/java/com/folioreader/activity/HighlightListActivity.java
@@ -0,0 +1,15 @@
+package com.folioreader.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.folioreader.R;
+
+public class HighlightListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highlight_list);
+    }
+}
