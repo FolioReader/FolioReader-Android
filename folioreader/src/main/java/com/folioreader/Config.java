e37fbc0581d8a11f80e5f729958178069b72ba17
avez raj <avez raj>
2017-08-22 19:25:41 +0530

    tts enable/disable implemented

--- folioreader/src/main/java/com/folioreader/Config.java
+++ folioreader/src/main/java/com/folioreader/Config.java
@@ -12,12 +12,14 @@ public class Config implements Parcelable {
     private int fontSize;
     private boolean nightMode;
     private int themeColor;
+    private boolean isTtsNeeded;
 
-    public Config(int font, int fontSize, boolean nightMode, int iconcolor) {
+    public Config(int font, int fontSize, boolean nightMode, int iconcolor, boolean isTtsNeeded) {
         this.font = font;
         this.fontSize = fontSize;
         this.nightMode = nightMode;
         this.themeColor = iconcolor;
+        this.isTtsNeeded = isTtsNeeded;
     }
 
     private Config(ConfigBuilder configBuilder) {
@@ -25,6 +27,7 @@ public class Config implements Parcelable {
         fontSize = configBuilder.fontSize;
         nightMode = configBuilder.nightMode;
         themeColor = configBuilder.themeColor;
+        isTtsNeeded = configBuilder.isTtsNeeded;
     }
 
     private Config() {
@@ -32,6 +35,7 @@ public class Config implements Parcelable {
         font = 3;
         nightMode = false;
         themeColor = R.color.app_green;
+        isTtsNeeded = true;
     }
 
     private Config(Parcel in) {
@@ -70,6 +74,14 @@ public class Config implements Parcelable {
 
     public void setThemeColor(int themeColor) {
         this.themeColor = themeColor;
+    }
+
+    public boolean isTtsNeeded() {
+        return isTtsNeeded;
+    }
+
+    public void setTtsNeeded(boolean ttsNeeded) {
+        isTtsNeeded = ttsNeeded;
     }
 
     @Override
@@ -114,6 +126,7 @@ public class Config implements Parcelable {
         dest.writeInt(fontSize);
         dest.writeInt(nightMode ? 1 : 0);
         dest.writeInt(themeColor);
+        dest.writeInt(isTtsNeeded ? 1 : 0);
     }
 
     private void readFromParcel(Parcel in) {
@@ -121,6 +134,7 @@ public class Config implements Parcelable {
         fontSize = in.readInt();
         nightMode = in.readInt() == 1;
         themeColor = in.readInt();
+        isTtsNeeded = in.readInt() == 1;
     }
 
     public static final Creator<Config> CREATOR = new Creator<Config>() {
@@ -140,6 +154,7 @@ public class Config implements Parcelable {
         private int fontSize = 2;
         private boolean nightMode = false;
         private int themeColor = R.color.app_green;
+        private boolean isTtsNeeded;
 
         public ConfigBuilder font(int font) {
             this.font = font;
@@ -160,6 +175,12 @@ public class Config implements Parcelable {
             this.themeColor = themeColor;
             return this;
         }
+
+        public ConfigBuilder isTtsNeeded(boolean isTttsneeded) {
+            this.isTtsNeeded = isTttsneeded;
+            return this;
+        }
+
 
         public Config build() {
             return new Config(this);
