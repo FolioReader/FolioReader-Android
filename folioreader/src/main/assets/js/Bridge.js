a6dd48f9879eb976cade7f00658184c106f177ad
Mahavir Jain <mahavir@codetoart.com>
2018-03-05 18:23:26 +0530

    horizontal scroll calculation improved

--- folioreader/src/main/assets/js/Bridge.js
+++ folioreader/src/main/assets/js/Bridge.js
@@ -275,16 +275,20 @@ function removeAllClasses(className) {
 function initializeHorizontalOrientation() {
     var d = document.getElementsByTagName('body')[0];
     var ourH = window.innerHeight;
-    var ourW = window.innerWidth;
+    var ourW = window.innerWidth - (2*20);
     var fullH = d.offsetHeight;
+    //var bodyW = ourW - 40;
     var pageCount = Math.floor(fullH/ourH)+1;
     var currentPage = 0;
-    var newW = pageCount*ourW;
+    var newW = pageCount*window.innerWidth;
     d.style.height = ourH+'px';
     d.style.width = newW+'px';
     d.style.margin = 0;
     d.style.webkitColumnCount = pageCount;
-    return "horizontalPageCount:" + pageCount;
+    d.style.webkitColumnGap = '40px';
+    d.style.webkitColumnWidth = ourW + 'px';
+    //d.style.webkitMarginCollapse = 'separate';
+    return "horizontalPageCount:10";
 }
 
 /**
