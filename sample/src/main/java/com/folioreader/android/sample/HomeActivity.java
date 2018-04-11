d02654382f5acf7bd4772c930599ec6e155744e5
Hrishikesh Kadam <hrishikesh@codetoart.com>
2018-04-11 15:29:16 +0530

    Delete user epub files

--- sample/src/main/java/com/folioreader/android/sample/HomeActivity.java
+++ sample/src/main/java/com/folioreader/android/sample/HomeActivity.java
@@ -50,7 +50,7 @@ public class HomeActivity extends AppCompatActivity implements OnHighlightListen
         findViewById(R.id.btn_raw).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                folioReader.openBook(R.raw.aayesha);
+                folioReader.openBook(R.raw.adventures);
             }
         });
 
