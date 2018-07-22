1bf9cdc0a2064a9dccefdd944e23a8d2e0aec9e3
Hrishikesh Kadam <hrkadam.92@gmail.com>
2018-07-23 04:35:28 +0530

    Distraction free reading mode for API 15

--- folioreader/src/main/java/com/folioreader/ui/folio/activity/FolioActivity.java
+++ folioreader/src/main/java/com/folioreader/ui/folio/activity/FolioActivity.java
@@ -16,7 +16,6 @@
 package com.folioreader.ui.folio.activity;
 
 import android.Manifest;
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -50,7 +49,6 @@ import com.folioreader.util.AppUtil;
 import com.folioreader.util.FileUtil;
 import com.folioreader.view.ConfigBottomSheetDialogFragment;
 import com.folioreader.view.DirectionalViewpager;
-import com.folioreader.view.FolioAppBarLayout;
 import com.folioreader.view.FolioToolbar;
 import com.folioreader.view.FolioToolbarCallback;
 import com.folioreader.view.MediaControllerCallback;
@@ -75,7 +73,7 @@ import static com.folioreader.Constants.TYPE;
 public class FolioActivity
         extends AppCompatActivity
         implements FolioActivityCallback, MainMvpView, MediaControllerCallback,
-        FolioToolbarCallback {
+        FolioToolbarCallback, View.OnSystemUiVisibilityChangeListener {
 
     private static final String LOG_TAG = "FolioActivity";
 
@@ -124,6 +122,7 @@ public class FolioActivity
         super.onCreate(savedInstanceState);
         handler = new Handler();
         setConfig(savedInstanceState);
+        initDistractionFreeMode(savedInstanceState);
 
         setContentView(R.layout.folio_activity);
         this.savedInstanceState = savedInstanceState;
@@ -140,7 +139,6 @@ public class FolioActivity
 
         initToolbar();
         initMediaController();
-        initDistractionFreeMode(savedInstanceState);
 
         if (ContextCompat.checkSelfPermission(FolioActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(FolioActivity.this, Constants.getWriteExternalStoragePerms(), Constants.WRITE_EXTERNAL_STORAGE_REQUEST);
@@ -244,21 +242,7 @@ public class FolioActivity
     public void initDistractionFreeMode(Bundle savedInstanceState) {
         Log.d(LOG_TAG, "-> initDistractionFreeMode");
 
-        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                Log.d(LOG_TAG, "-> onSystemUiVisibilityChange -> visibility = " + visibility);
-
-                distractionFreeMode = visibility != View.SYSTEM_UI_FLAG_VISIBLE;
-                Log.d(LOG_TAG, "-> distractionFreeMode = " + distractionFreeMode);
-
-                if (distractionFreeMode) {
-                    actionBar.hide();
-                } else {
-                    actionBar.show();
-                }
-            }
-        });
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(this);
 
         // Deliberately Hidden and shown to make activity contents laid out behind SystemUI
         hideSystemUI();
@@ -278,6 +262,22 @@ public class FolioActivity
     }
 
     @Override
+    public void onSystemUiVisibilityChange(int visibility) {
+        Log.d(LOG_TAG, "-> onSystemUiVisibilityChange -> visibility = " + visibility);
+
+        distractionFreeMode = visibility != View.SYSTEM_UI_FLAG_VISIBLE;
+        Log.d(LOG_TAG, "-> distractionFreeMode = " + distractionFreeMode);
+
+        if (actionBar != null) {
+            if (distractionFreeMode) {
+                actionBar.hide();
+            } else {
+                actionBar.show();
+            }
+        }
+    }
+
+    @Override
     public void toggleSystemUI() {
 
         if (distractionFreeMode) {
@@ -287,30 +287,37 @@ public class FolioActivity
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public void showSystemUI() {
         Log.d(LOG_TAG, "-> showSystemUI");
 
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= 16) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        } else {
+            onSystemUiVisibilityChange(View.SYSTEM_UI_FLAG_VISIBLE);
+        }
     }
 
-    @TargetApi(Build.VERSION_CODES.KITKAT)
     public void hideSystemUI() {
         Log.d(LOG_TAG, "-> hideSystemUI");
 
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
-                // Set the content to appear under the system bars so that the
-                // content doesn't resize when the system bars hide and show.
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= 16) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    // Hide the nav bar and status bar
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        } else {
+            // Specified 1 just to mock anything other than View.SYSTEM_UI_FLAG_VISIBLE
+            onSystemUiVisibilityChange(1);
+        }
     }
 
     @Override
